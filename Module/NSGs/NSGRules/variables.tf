0f10e57274f2ad9f61f87d65eec30525f0a19c3a
Kooragayala <k.a.prashanth@accenture.com>
2021-04-05 23:19:09 +0530

    modified variable file to accept string

--- Module/NSGs/NSGRules/variables.tf
+++ Module/NSGs/NSGRules/variables.tf
@@ -1,17 +1,22 @@
 variable "location" {
+  type        = string
   description = "Location for all resources"
 }
 
 variable "networkrgname" {
+  type        = string
   description = "Location for all resources"
 }
 
 variable "hao-test-rg_name" {
+  type        = string
   description = "Name of NSG RG"
 }
 variable "hao_mgmt_nsg_name" {
+  type        = string
   description = "Name of NSG"
 }
 variable "hao_mgmt_rules" {
+  type        = string
   description = "Open SSH to Hub management network"
 }
