aebfb2770b13232c80ffe9b4bc038fe563fb681e
Kooragayala <k.a.prashanth@accenture.com>
2021-04-06 12:01:22 +0530

    modified main code to module based and also variable segregated

--- Module/NSGs/NSGRules/main.tf
+++ Module/NSGs/NSGRules/main.tf
@@ -14,17 +14,16 @@ resource "azurerm_network_security_group" "hao_mgmt_nsg" {
 }
 
 resource "azurerm_network_security_rule" "nsg_hao_rules" {
-  name                          = lookup(var.hao_mgmt_rules[count.index], "name", "defaultruleplaceholder")
-  priority                      = lookup(var.hao_mgmt_rules[count.index], "priority", 64999)
-  direction                     = lookup(var.hao_mgmt_rules[count.index], "direction", "Inbound")
-  access                        = lookup(var.hao_mgmt_rules[count.index], "access", "Deny")
-  protocol                      = lookup(var.hao_mgmt_rules[count.index], "protocol", "udp")
-  source_port_range             = lookup(var.hao_mgmt_rules[count.index], "source_port_ranges", "8888")
-  source_address_prefix         = lookup(var.hao_mgmt_rules[count.index], "source_address_prefix", "8.8.8.8/32")
-  destination_port_range        = lookup(var.hao_mgmt_rules[count.index], "destination_port_ranges", "8888")
-  destination_address_prefix    = lookup(var.hao_mgmt_rules[count.index], "destination_address_prefix", "8.8.8.8/32")
-  description                   = lookup(var.hao_mgmt_rules[count.index], "description", "defaultplaceholder")
+  name                          = var.rule_name[count.index], "name", "defaultruleplaceholder"
+  priority                      = var.rule_priority[count.index], "priority", 64999
+  direction                     = var.rule_direction[count.index], "direction", "Inbound"
+  access                        = var.rule_access[count.index], "access", "Deny"
+  protocol                      = var.rule_protocol[count.index], "protocol", "udp"
+  source_port_range             = var.source_port_ranges[count.index], "source_port_ranges", "8888"
+  source_address_prefix         = var.source_address_prefix[count.index], "source_address_prefix", "8.8.8.8/32"
+  destination_port_range        = var.destination_port_ranges[count.index], "destination_port_ranges", "8888"
+  destination_address_prefix    = var.destination_address_prefix[count.index], "destination_address_prefix", "8.8.8.8/32"
+  description                   = var.rule_description[count.index], "description", "defaultplaceholder"
   resource_group_name           = azurerm_resource_group.nsg_rg.name
   network_security_group_name   = azurerm_network_security_group.hao_mgmt_nsg.name
-  count                         = length(var.hao_mgmt_rules)
 }
--- Module/NSGs/NSGRules/variables.tf
+++ Module/NSGs/NSGRules/variables.tf
@@ -2,12 +2,6 @@ variable "location" {
   type        = string
   description = "Location for all resources"
 }
-
-variable "networkrgname" {
-  type        = string
-  description = "Location for all resources"
-}
-
 variable "hao-test-rg_name" {
   type        = string
   description = "Name of NSG RG"
@@ -16,7 +10,44 @@ variable "hao_mgmt_nsg_name" {
   type        = string
   description = "Name of NSG"
 }
-variable "hao_mgmt_rules" {
+variable "rule_name" {
   type        = string
-  description = "Open SSH to Hub management network"
+  description = "Name for NSG rules"
+}
+variable "rule_priority" {
+  type        = string
+  description = "Priority of the Rules"
+}
+variable "rule_direction" {
+  type        = string
+  description = "Direction for Rules"
+}
+variable "rule_access" {
+  type        = string
+  description = "Access for rule Allow/Deny"
+}
+variable "rule_protocol" {
+  type        = string
+  description = "Portocal for NSG Rules"
+}
+variable "source_port_ranges" {
+  type        = string
+  description = "Port range for source IP"
+}
+variable "source_address_prefix" {
+  type        = string
+  description = "Source address Prefix"
+}
+variable "destination_port_ranges" {
+  type        = string
+  description = "Port range for destination IP"
+}
+
+variable "destination_address_prefix" {
+  type        = string
+  description = "Destination address Prefix"
+}
+variable "rule_description" {
+  type        = string
+  description = "Description for rules"
 }
