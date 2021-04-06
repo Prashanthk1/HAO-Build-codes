e8353e4b70154cd4fc8652ef89ea936e141437ee
Prashanthk1 <78205389+Prashanthk1@users.noreply.github.com>
2021-04-06 09:48:13 +0530

    Update variables.tf

--- Module/NSGs/NSGRules/variables.tf
+++ Module/NSGs/NSGRules/variables.tf
@@ -1,17 +1,36 @@
-variable "location" {
+ariable "location" {
+  type = "string"
   description = "Location for all resources"
 }
 
 variable "networkrgname" {
+  type = "string"
   description = "Location for all resources"
 }
 
 variable "hao-test-rg_name" {
+  type = "string"
   description = "Name of NSG RG"
 }
+
 variable "hao_mgmt_nsg_name" {
+  type = "string"
   description = "Name of NSG"
 }
+
 variable "hao_mgmt_rules" {
+  type = object (
+    name                        = "string"
+    priority                    = "string"
+    direction                   = "string"
+    access                      = "string"
+    protocol                    = "string"
+    source_port_ranges          = "string"
+    source_address_prefix       = "string"
+    destination_port_ranges     = "string"
+    destination_address_prefix  = "string"
+    description                 = "string"
+
+  )
   description = "Open SSH to Hub management network"
 }
