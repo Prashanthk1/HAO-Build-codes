2ce30467255c4a6ce1e54bd0cef160f559d1a9cd
Prashanthk1 <78205389+Prashanthk1@users.noreply.github.com>
2021-04-06 12:31:18 +0530

    Update variables.tf

--- Module/NSGs/NSGRules/variables.tf
+++ Module/NSGs/NSGRules/variables.tf
@@ -1,52 +1,52 @@
 variable "location" {
-  type        = string
+  type        = "string"
   description = "Location for all resources"
 }
 variable "hao-test-rg_name" {
-  type        = string
+  type        = "string"
   description = "Name of NSG RG"
 }
 variable "hao_mgmt_nsg_name" {
-  type        = string
+  type        = "string"
   description = "Name of NSG"
 }
 variable "rule_name" {
-  type        = string
+  type        = "string"
   description = "Name for NSG rules"
 }
 variable "rule_priority" {
-  type        = string
+  type        = "string"
   description = "Priority of the Rules"
 }
 variable "rule_direction" {
-  type        = string
+  type        = "string"
   description = "Direction for Rules"
 }
 variable "rule_access" {
-  type        = string
+  type        = "string"
   description = "Access for rule Allow/Deny"
 }
 variable "rule_protocol" {
-  type        = string
+  type        = "string"
   description = "Portocal for NSG Rules"
 }
 variable "source_port_ranges" {
-  type        = string
+  type        = "string"
   description = "Port range for source IP"
 }
 variable "source_address_prefix" {
-  type        = string
+  type        = "string"
   description = "Source address Prefix"
 }
 variable "destination_port_ranges" {
-  type        = string
+  type        = "string"
   description = "Port range for destination IP"
 }
 variable "destination_address_prefix" {
-  type        = string
+  type        = "string"
   description = "Destination address Prefix"
 }
 variable "rule_description" {
-  type        = string
+  type        = "string"
   description = "Description for rules"
 }
